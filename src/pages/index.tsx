8efc88f47198260609169014ce7fd5f6186af789
hidebug <hidebug@qq.com>
2024-03-31 19:55:42 +0800

    修改注释

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import NextLink from "next/link"
-import { VStack, Heading, Box, LinkOverlay, LinkBox} from "@chakra-ui/layout"
+import { VStack, Heading, Box, LinkOverlay, LinkBox } from "@chakra-ui/layout"
 import { Text, Button } from '@chakra-ui/react'
-import { useState, useEffect} from 'react'
-import {ethers} from "ethers"
+import { useState, useEffect } from 'react'
+import { ethers } from "ethers"
 import ReadERC20 from "components/ReadERC20"
 import TransferERC20 from "components/TransferERC20"
 
-declare let window:any
+declare let window: any
 const addressERC20 = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 
 const Home: NextPage = () => {
@@ -20,30 +20,30 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     //get ETH balance and network info only when having currentAccount 
-    if(!currentAccount || !ethers.utils.isAddress(currentAccount)) return
+    if (!currentAccount || !ethers.utils.isAddress(currentAccount)) return
 
     //client side code
-    if(!window.ethereum) {
+    if (!window.ethereum) {
       console.log("please install MetaMask")
       return
     }
 
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-    provider.getBalance(currentAccount).then((result)=>{
+    provider.getBalance(currentAccount).then((result) => {
       setBalance(ethers.utils.formatEther(result))
-    }).catch((e)=>console.log(e))
+    }).catch((e) => console.log(e))
 
-    provider.getNetwork().then((result)=>{
+    provider.getNetwork().then((result) => {
       setChainId(result.chainId)
       setChainName(result.name)
-    }).catch((e)=>console.log(e))
+    }).catch((e) => console.log(e))
 
-  },[currentAccount])
+  }, [currentAccount])
 
   //click connect
   const onClickConnect = () => {
     //client side code
-    if(!window.ethereum) {
+    if (!window.ethereum) {
       console.log("please install MetaMask")
       return
     }
@@ -60,9 +60,9 @@ const Home: NextPage = () => {
     //we can do it using ethers.js
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     provider.send("eth_requestAccounts", [])
-    .then((accounts)=>{
-      if(accounts.length>0) setCurrentAccount(accounts[0])
-    }).catch((e)=>console.log(e))
+      .then((accounts) => {
+        if (accounts.length > 0) setCurrentAccount(accounts[0])
+      }).catch((e) => console.log(e))
 
   }
 
@@ -79,38 +79,39 @@ const Home: NextPage = () => {
         <title>My DAPP</title>
       </Head>
 
-      <Heading as="h3"  my={4}>Explore Web3</Heading>          
+      <Heading as="h3" my={4}>Explore Web3</Heading>
+      
       <VStack>
         <Box w='100%' my={4}>
-        {currentAccount  
-          ? <Button type="button" w='100%' onClick={onClickDisconnect}>
-                Account:{currentAccount}
+          {currentAccount
+            ? <Button type="button" w='100%' onClick={onClickDisconnect}>
+              Account:{currentAccount}
             </Button>
-          : <Button type="button" w='100%' onClick={onClickConnect}>
-                  Connect MetaMask
-              </Button>
-        }
+            : <Button type="button" w='100%' onClick={onClickConnect}>
+              Connect MetaMask
+            </Button>
+          }
         </Box>
-        {currentAccount  
-          ?<Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
-          <Heading my={4}  fontSize='xl'>Account info</Heading>
-          <Text>ETH Balance of current account: {balance}</Text>
-          <Text>Chain Info: ChainId {chainId} name {chainname}</Text>
-        </Box>
-        :<></>
+        {currentAccount
+          ? <Box mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
+            <Heading my={4} fontSize='xl'>Account info</Heading>
+            <Text>ETH Balance of current account: {balance}</Text>
+            <Text>Chain Info: ChainId {chainId} name {chainname}</Text>
+          </Box>
+          : <></>
         }
 
-        <Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
-          <Heading my={4}  fontSize='xl'>Read ClassToken Info</Heading>
-          <ReadERC20 
+        <Box mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
+          <Heading my={4} fontSize='xl'>Read ClassToken Info</Heading>
+          <ReadERC20
             addressContract={addressERC20}
             currentAccount={currentAccount}
           />
         </Box>
 
-        <Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
-          <Heading my={4}  fontSize='xl'>Transfer Classtoken</Heading>
-          <TransferERC20 
+        <Box mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
+          <Heading my={4} fontSize='xl'>Transfer Classtoken</Heading>
+          <TransferERC20
             addressContract={addressERC20}
             currentAccount={currentAccount}
           />
